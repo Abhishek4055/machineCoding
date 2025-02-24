ad37d3c9186c75651e1737bc06b4d0c40ecd0bbf
Abhihsek <abhishekanand.jnv@gmail.com>
2025-02-24 14:55:16 +0530

    .

--- src/redux/store.js
+++
@@ -1,8 +0,0 @@
-import { configureStore } from "@reduxjs/toolkit";
-import { somethingReducer } from "./somethingSclice";
-
-const centeralizedStore = configureStore({
-  reducer: {
-    cart: somethingReducer, // thsi is a reducer
-  },
-});
