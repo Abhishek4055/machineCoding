1b48e2943ccab0182f9ee2713b6090f7139bc981
Abhihsek <abhishekanand.jnv@gmail.com>
2025-04-03 17:12:04 +0530

    to Find Set of Anangram

---
+++ src/my coding/components/ToDoThis/FindSetOfAnagram.js
@@ -0,0 +1,65 @@
+import React from "react";
+
+function FindSetOfAnagram() {
+  const input = ["eat", "tea", "tan", "ate", "nat", "bat"];
+
+  function outPut(arr) {
+    if (!arr.length) return "No Item found";
+    let result = new Map();
+
+    for (let item of arr) {
+      const sortedString = item.split("").sort().join("");
+      if (result.has(sortedString)) {
+        result.get(sortedString).push(item);
+      } else {
+        result.set(sortedString, [item]);
+      }
+    }
+
+    return [...result.values()];
+  }
+
+  const outCome = outPut(input);
+
+  const solution = `
+  
+    function outPut(arr) {
+
+    if (!arr.length) return "No Item found";
+
+    let result = new Map();
+    for (let item of arr) {
+      const sortedString = item.split("").sort().join("");
+      if (result.has(sortedString)) {
+        result.get(sortedString).push(item);
+      } else {
+        result.set(sortedString, [item]);
+      }
+    }
+
+        return [...result.values()];
+    }
+
+    const outCome = outPut(input);
+
+    `;
+
+  return (
+    <div>
+      <h3>Find the sets of Anagram</h3>
+      <p>
+        <strong>Input:</strong> {JSON.stringify(input)}
+      </p>
+      <p>
+        <strong>Output:</strong> {JSON.stringify(outCome)}
+      </p>
+
+      <h2>Solution:</h2>
+      <pre>
+        <code>{solution}</code>
+      </pre>
+    </div>
+  );
+}
+
+export default FindSetOfAnagram;
